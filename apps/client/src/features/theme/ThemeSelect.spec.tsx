f8cb7fa2763ea31d0466ee0cdc1270999a2c9106
Ben Kinsey <bkinsey@gmail.com>
2021-08-14 09:15:39 -1000

    fix test

--- apps/client/src/features/theme/ThemeSelect.spec.tsx
+++ apps/client/src/features/theme/ThemeSelect.spec.tsx
@@ -1,6 +1,6 @@
 import { MockedProvider } from '@apollo/client/testing';
 
-import { fireEvent, render, screen, within } from '@testing-library/react';
+import { act, fireEvent, render, screen, within } from '@testing-library/react';
 
 import { themeChoiceVar, cache } from '../../cache';
 
@@ -16,28 +16,38 @@ describe('ThemeSelect', () => {
     const wrapperNode = screen.getByLabelText('Theme');
     const button = within(wrapperNode).getByRole('button');
     expect(button).toBeDefined();
-    fireEvent.mouseDown(button);
+    act(() => {
+      fireEvent.mouseDown(button);
+    });
     const listbox = screen.getByRole('listbox');
     const lightOption = within(listbox).getByText('Light');
-    fireEvent.click(lightOption);
+
+    act(() => {
+      fireEvent.click(lightOption);
+    });
     expect(themeChoiceVar()).toBe('Light');
   });
 
   it('should set dark mode successfully', () => {
-    render(
-      <MockedProvider cache={cache}>
-        <ThemeSelect />
-      </MockedProvider>
-    );
+    act(() => {
+      render(
+        <MockedProvider cache={cache}>
+          <ThemeSelect />
+        </MockedProvider>
+      );
+    });
     const wrapperNode = screen.getByLabelText('Theme');
     const button = within(wrapperNode).getByRole('button');
     expect(button).toBeDefined();
-    fireEvent.mouseDown(button);
+    act(() => {
+      fireEvent.mouseDown(button);
+    });
     const listbox = screen.getByRole('listbox');
     const darkOption = within(listbox).getByText('Dark');
-    fireEvent.click(darkOption);
+
+    act(() => {
+      fireEvent.click(darkOption);
+    });
     expect(themeChoiceVar()).toBe('Dark');
   });
 });
-
-// github copilot next suggestion keyboard shortcut is Alt
