bc244d86d19d4d3f1eddd43457b11f64aecd6409
Ben Kinsey <bkinsey@gmail.com>
2021-09-12 09:21:43 -1000

    remove console log

--- apps/client/src/app/urqlClient.ts
+++ apps/client/src/app/urqlClient.ts
@@ -5,8 +5,6 @@ import { createClient, dedupExchange, fetchExchange } from 'urql';
 
 import schema from '../../../../schema.json';
 
-console.log('called once');
-
 export const urqlClient = createClient({
   url: 'http://localhost:4000/graphql',
   exchanges: [
